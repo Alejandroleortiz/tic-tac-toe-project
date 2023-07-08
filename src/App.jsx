2a3a041435263d3cf5de227871f7b3f26e1ab2d8
Alejandro Leon <alejandroleortiz57@gmail.com>
2023-07-08 00:59:15 -0400

    culminado turnos y cuadrantes para jugar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 
 const TURNS = {
@@ -5,29 +6,70 @@ const TURNS = {
   O: 'o'
 }
 
-const board = Array(9).fill(null)
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
 
 function App() {
+  const [board, setBoard] = useState(
+    Array(9).fill(null)  //const board = Array(9).fill(null) // Creo un array con 9 espacios vacios y con el valor null
+  ) // Defino el estado para la logica al hacer clic
+
+  const [turn, setTurn] = useState(TURNS.X) // estado para saber de quien es el turno
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn // guardar el turno x u o
+    setBoard(newBoard)
+    
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+
       <section className="game">
         {
           board.map((_, index) => {
 
             return (
-              <div
-                className="cell"
-                key={index}>
-                <span
-                  className="cell__content">
-                  {index}
-                </span>
-              </div>
-              )
+
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
           })
         }
       </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+
     </main>
   )
 }
